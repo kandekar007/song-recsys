fd54f362bd656194d234ffa70e5762776fa2e1ac
kandekar.007 <kandekar@iitg.ac.in>
2021-07-18 00:01:29 +0530

    changed front

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
-    search = StringField("", validators = [DataRequired()])
-    submit = SubmitField("Get recommendations")
+    search = StringField("", validators = [DataRequired()], render_kw={"placeholder": "Tell us a song stuck in your head <3"})
+    submit = SubmitField("Search for similar songs")
